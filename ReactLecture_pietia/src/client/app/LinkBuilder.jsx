4db874683e15c79b4f72c7914d8229bc924a541d
sametcem <50104561+sametcem@users.noreply.github.com>
2019-12-09 10:15:43 +0100

    Delete LinkBuilder.jsx

--- ReactLecture_pietia/src/client/app/LinkBuilder.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class LinkBuilder extends React.Component {
-    render() {
-        return <a href={this.props.url} title={this.props.label} target="_blank">{this.props.label}</a>
-    }
-}
-
-export default LinkBuilder;
